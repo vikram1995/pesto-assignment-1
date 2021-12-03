26ad40ea22f25487f6f36ef25c45d6f188313321
vikram kumar <vikram.1rn14ec170@gmail.com>
2021-12-03 08:31:33 +0530

    Add loading spinner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import UserListPage from './Components/UserListPage/UserListPage'
+import LoadingSpinner from './Components/LoadingSpinner/LoadingSpinner'
 import {
   BrowserRouter,
   Routes,
@@ -11,7 +12,6 @@ import UserDetailsPage from './Components/UserDetailsPage/UserDetailsPage';
 
 function App() {
   const [usersList, setUsersList] = useState(null)
-  const [errorMsg, setErrorMsg] = useState(null)
   useEffect(() => {
     try {
       fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(data => {
@@ -19,19 +19,21 @@ function App() {
       setUsersList(data);
     })
     } catch (error) {
-      setErrorMsg("Failed to fetch data from server")
+      console.log("falied to fetch data from server")
     }
     
   }, [])
 
   return (
-    <BrowserRouter>
+    <div>
+      {!usersList && <LoadingSpinner />}
+      <BrowserRouter>
       <Routes>
         <Route path="/" element={usersList && <UserListPage usersList={usersList}/> } />
         <Route path="user/:id" element={usersList &&  <UserDetailsPage usersList={usersList}/>} />
       </Routes>
     </BrowserRouter>
-
+    </div>
   );
 }
 
